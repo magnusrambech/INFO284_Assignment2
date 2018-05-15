b32d1b7c3a1e7344415fe54ee6c6cd8cafdd141e
Magnus Rambech <Magnusrambech@msn.com>
2018-05-15 11:26:22 +0200

    Imported numpy,sklearn, mpl, GaussianMixture and k_means. included and read dataset file

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,23 @@
 import sklearn
 import numpy
+import matplotlib.pyplot as plt
 from sklearn import datasets
-iris = datasets.load_iris()
-digits = datasets.load_digits()
+from sklearn import svm
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import k_means
+#digits = datasets.load_digits()
 
-print(digits.data)
+#clf = svm.SVC(gamma=0.001, C=100)
+#x,y = digits.data[:-10],digits.target[:-10]
+
+#clf.fit(x,y)
+
+#print("Prediction of last:",clf.predict(digits.data[[-2]]))
+#plt.imshow(digits.images[-2], cmap = plt.cm.gray_r, interpolation="nearest")
+#plt.show()
+
+
+print("____________________Assignment 2_________________________")
+
+with open("seeds_dataset.txt","r") as myFile:
+    data=myFile.read()
