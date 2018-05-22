b5c1497a0b23a66eb9503e084756fc64bdd7602d
magnusrambech <magnusrambech@msn.com>
2018-05-22 16:34:07 +0200

    kmeans cluster
    TODO:
    Visualize
    GMM

--- venv/main.py
+++ venv/main.py
@@ -6,30 +6,28 @@ from scipy import stats
 from sklearn import datasets
 from sklearn import svm
 from sklearn.mixture import GMM
-from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
-#digits = datasets.load_digits()
 
-#clf = svm.SVC(gamma=0.001, C=100)
-#x,y = digits.data[:-10],digits.target[:-10]
 
-#clf.fit(x,y)
-
-#print("Prediction of last:",clf.predict(digits.data[[-2]]))
-#plt.imshow(digits.images[-2], cmap = plt.cm.gray_r, interpolation="nearest")
-#plt.show()
 
 
 print("____________________Assignment 2__________________________")
 
-with open("seeds_dataset.txt","r") as myFile:
-    data = myFile.read()
+data = pd.read_csv('seeds_dataset.txt', sep="\t", error_bad_lines=False)
 
-#print(data)
+kmeans = KMeans(n_clusters=3, random_state=0).fit(data)
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
 
-nData = pd.read_csv('seeds_dataset.txt', sep='\t')
-print(nData)
+data['clusters'] = labels
+
+colors = ["g.", "r.", "b."]
+
+
+
+#print(data.groupby(['clusters']).mean())
+#print(centroids)
 
 
 
